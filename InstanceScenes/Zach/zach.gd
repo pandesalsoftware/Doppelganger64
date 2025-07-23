52bc51b3c0724bd9deb87fe83729a799b94fa291
Mars <pandesalsoftware@gmail.com>
2025-07-23 14:37:24 -0400

    Rude Zach
    Zach is fixed ready to be possibly instanced after a dummy Zach starts the quest and queue free's himself before instancing the Follower Quest Zach to the Characters organizational Node3D in the main scene.

---
+++ InstanceScenes/Zach/zach.gd
@@ -0,0 +1,22 @@
+extends CharacterBody3D
+
+@onready var NavAgent = $NavigationAgent3D
+@onready var ZS_AP = $ZachSkin/AnimationPlayer
+@onready var ZS : Node3D =  %ZachSkin
+
+@export var move_speed = 8
+
+func _physics_process(delta):
+	var current_location = global_transform.origin
+	var next_location = NavAgent.get_next_path_position()
+	var new_velocity = (next_location - current_location).normalized() * move_speed
+	
+	velocity = velocity.move_toward(new_velocity, .25)
+	move_and_slide()
+	ZS.look_at(next_location)
+	ZS.rotate_object_local(Vector3.UP, PI)
+	ZS_AP.play("Walk")
+	
+
+func _update_target_location(target_location):
+	NavAgent.set_target_position(target_location)
--- NavigationScenes/Levels/Level_Scripts/starting_room.gd
+++ NavigationScenes/Levels/Level_Scripts/starting_room.gd
@@ -7,3 +7,4 @@ extends Node3D
 #Weird error but not game breaking...If switched to alternate script The Doppelganger doesn't go to Cooper as intended. 
 func _physics_process(delta):
 	get_tree().call_group("Enemies", "_update_target_location", Cooper.global_transform.origin)
+	get_tree().call_group("NPCs", "_update_target_location", Cooper.global_transform.origin)
