98206c38269d0eee564c1cb6eaa6e235ad658b07
Mars <pandesalsoftware@gmail.com>
2025-07-28 12:46:55 -0400

    Quest System Signals done, Needs Cutscene and Text Scroll

--- NavigationScenes/Levels/YorkQuestBegin.gd
+++ NavigationScenes/Levels/YorkQuestBegin.gd
@@ -8,12 +8,16 @@ func _enter_radius(body):
 	if body.name == "Cooper":
 		in_radius = true 
 	if in_radius:
-		print("In radius!")
+		print("Cooper entered York radius!")
+		$"../InteractPrompt".visible = true 
 
 func _exit_radius(body):
 	if body.name == "Cooper":
 		in_radius = false
+		print("Cooper exited York radius!")
+		$"../InteractPrompt".visible = false
 
-func _physics_process(_delta: float)  ->  void:
-	if in_radius && get_parent().current != true:
-		get_parent().current = true
+
+func _physics_process(delta):
+	if $"../InteractPrompt".visible && Input.is_action_just_pressed("Interact"): 
+		print("Starting York Quest")
