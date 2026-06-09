cf0a900468602d21cb7275983846c280990c7964
Mars <pandesalsoftware@gmail.com>
2026-06-09 17:04:12 -0400

    Map Changes

--- NavigationScenes/MainMenu/in_memoriam.gd
+++ NavigationScenes/MainMenu/in_memoriam.gd
@@ -1,7 +1,19 @@
 extends Node3D
 
 @onready var C_AP = $Camera3D/AnimationPlayer
+@onready var T_AP = $"2D/Tribute/AnimationPlayer"
+
+var gameStart = false 
+
+func _on_animation_player_animation_finished(anim_name):
+	if !gameStart and anim_name == "Move":
+		$Camera3D/AnimationPlayer.play("Sway")
+		
+	if gameStart and anim_name == "Fade_2":
+		get_tree().change_scene_to_file("res://NavigationScenes/Levels/starting_room.tscn")
 
 
-func _on_animation_player_animation_finished(Move):
-	$Camera3D/AnimationPlayer.play("Sway")
+func _on_play_pressed() -> void:
+	gameStart = true 
+	C_AP.play("Move_2")
+	T_AP.play("Fade_2")
